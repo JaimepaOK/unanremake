4caa94878b8352f8922be0d9c57a1f1c98142bd0
Jaime Daniel Paredes Luna <45341285+JaimepaOK@users.noreply.github.com>
2021-10-12 10:18:38 -0600

    primeros pasos leaning

---
+++ unan/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="body">
+    <div class="header">hi :3</div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.body {
+  font-family: Arial, Helvetica, sans-serif;
+  background: linear-gradient(rgb(49, 36, 36), rgb(174, 38, 38));
+  padding: 20px;
+  border-radius: 45px;
+}
+.header {
+  font-size: 20px;
+  text-align: center;
+  border-radius: 25px;
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.645);
+  padding: 25px;
+  margin: 15px 120px 15px 120px;
+}
+</style>
---
+++ unan/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+
+import App from './App.vue'
+import cards from 'components/cards.vue'
+
+createApp(App).mount('#app')
+createApp(cards).mount('#cardex')
