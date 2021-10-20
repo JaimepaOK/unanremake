cbcd4dece98414650e30a47d69b94ae9036d9e73
Jaime Daniel Paredes Luna <45341285+JaimepaOK@users.noreply.github.com>
2021-10-19 22:08:55 -0600

    start db conect

---
+++ models/db.js
@@ -0,0 +1,22 @@
+const { Pool } = require('pg');
+
+const config = {
+    user: 'postgres',
+    host: 'localhost',
+    password: 'Link2000',
+    database: 'unantest',
+    port: '5432'
+};
+
+const pool = new Pool(config);
+
+const getNombres = async() => {
+    try {
+        const res = await pool.query('select nombre from cunan order by "ID"');
+        console.log(res.rows);
+
+    } catch (e) {
+        console.log(e);
+    }
+
+};
--- src/components/cards.vue
+++ src/components/cards.vue
@@ -4,7 +4,7 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+     
     </p>
   </div>
 </template>
@@ -18,7 +18,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
