0a25af871ad0efb3855135904abc001d16905966
OfilioHA <herdocia20@gmail.com>
2021-10-20 22:28:54 -0600

    Anadido Vuestic (UI Framework).

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <router-link to="/" >Home</router-link>  
-  <router-link to="/about" >about</router-link>
-  <router-view />  
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <va-navbar class="mb-2">
+    <template #left>
+      <va-navbar-item>LOGO HERE</va-navbar-item>
+    </template>
+    <template #center>
+      <va-navbar-item>Default color (secondary)</va-navbar-item>
+    </template>
+    <template #right>
+      <va-navbar-item>
+        <router-link to="/">Home</router-link>
+      </va-navbar-item>
+      <va-navbar-item>
+        <router-link to="/about">About</router-link>
+      </va-navbar-item>
+    </template>
+  </va-navbar>
+
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <router-link to="/">Home</router-link>
+  <router-link to="/about">about</router-link>
+  <router-view />
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import { VaNavbar, VaNavbarItem } from "vuestic-ui";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    VaNavbar,
+    VaNavbarItem,
+  },
+};
 </script>
 
 <style>
@@ -24,6 +43,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /*margin-top: 60px;*/
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,21 @@
 import { createApp } from 'vue'
 
 import { createStore } from 'vuex'
-import { createRouter, createWebHistory } from 'vue-router' 
+import { createRouter, createWebHistory } from 'vue-router'
 
 import Vuex from './VueVuex';
 import { routes } from './VueRouter';
 import AppComponent from './App.vue'
+import { VuesticPlugin } from 'vuestic-ui'
+import 'vuestic-ui/dist/vuestic-ui.css'
 
 const store = createStore(Vuex);
 const router = createRouter({history: createWebHistory(),routes});
 const app = createApp(AppComponent)
 app.use(router);
 app.use(store);
+
+app.use(VuesticPlugin);
 app.mount('#app')  
 
 
